0bb35a86e676d64be1268fc1a944615ad95388f1
MisterSupr69 <120346181+MisterSupr69@users.noreply.github.com>
2022-12-12 04:11:18 +0100

    Update menu_smb.ps1

--- menu_smb.ps1
+++ menu_smb.ps1
@@ -1,6 +1,5 @@
 function menu 
-{
-   
+{ 
    [int]$repuesta= Read-Host "
     `n`####################################################################################################################
     `n`#                                                                                                                  #
@@ -10,13 +9,15 @@ function menu
     `n`#                                                                                                                  #
     `n`####################################################################################################################
     `n` Elige una opcion: 
-    `n` 1: Habilitar Protocolo SMB1 (Requiere reinicio una vez ejecutado)
-    `n` 2: Desabilitar Protocolo SMB2
-    `n` 3: Habilita Protocolo SMB2
-    `n` 4: Instalacion y configuracion carpeta scanner (creacion carpeta c:/scanner crea acceso directo escritorio y comparte carpeta SMB)
-    `n` 5: Dejar de compartir la carpeta scanner
-    `n` 6: Salir
-    `n` Opcion"
+    `n` 1: Habilitar Protocolo SMB1 (Requiere reinicio una vez ejecutado).
+    `n` 2: Desabilitar Protocolo SMB2.
+    `n` 3: Habilita Protocolo SMB2.
+    `n` 4: Instalacion y configuración carpeta scanner (creacion carpeta c:/scanner crea acceso directo escritorio y comparte carpeta SMB).
+    `n` 5: Modificacion registro (veryoneincludesanonymous=0,NoLmHash=1 y restrictnullsessaccess=0)Si da problemas para quitar uso compatido por contraseña.
+    `n` 6: Dejar de compartir la carpeta scanner.
+    `n` 7: Ejecucion hostname (para saber nombre del equipo de red ponerlo en la impresora).
+    `n` 8: Salir.
+    `n` Opción"
    switch ($repuesta)
    {
        1{Enable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol}
@@ -32,17 +33,26 @@ function menu
            $Shortcut.TargetPath = "C:\scanner" 
            $Shortcut.Save()
        }
-       5{Remove-SmbShare -Name scanner}
-       6{exit}
+       5{
+         reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Lsa /v everyoneincludesanonymous /t REG_DWORD /d 0 /f
+         reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Lsa /v NoLmHash /t REG_DWORD /d 1 /f
+         reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\LanManServer\Parameters /v restrictnullsessaccess /t REG_DWORD /d 0 /f
+         }
+       6{Remove-SmbShare -Name scanner}
+       7{hostname}
+       8{exit}
        Default {Write-Host "Opcion no valida"}
     }
    
     
 }
 
-    do
-    {
+do
+{
+
         clear
         menu
         Read-Host "Pulsa intro para seguir"
-    }while ($true)
+    
+}
+while ($true)
