61cb9a676496ad8b58f112a0f961060ae099fc38
MisterSupr69 <120346181+MisterSupr69@users.noreply.github.com>
2023-10-11 02:52:42 +0200

    Update menu_smb.ps1

--- menu_smb.ps1
+++ menu_smb.ps1
@@ -8,7 +8,12 @@ function menu
     `n`#                                        Echo por @MisterSupr69 2023                                               #
     `n`#                                                                                                                  #
     `n`####################################################################################################################
-    `n` Elige una opcion: 
+    `n` Elige una opcion:
+    `n`####################################################################################################################
+    `n`#                                                                                                                  #
+    `n`#                                         OPCIONES_CARPETAS_SCANAER (1-7).                                         #
+    `n`#                                                                                                                  #
+    `n`####################################################################################################################
     `n` 1: Habilitar Protocolo SMB1 (Requiere reinicio una vez ejecutado).
     `n` 2: Desabilitar Protocolo SMB2.
     `n` 3: Habilita Protocolo SMB2.
@@ -16,7 +21,17 @@ function menu
     `n` 5: Modificacion registro (veryoneincludesanonymous=0,NoLmHash=1 y restrictnullsessaccess=0)Si da problemas para quitar uso compatido por passwod.
     `n` 6: Dejar de compartir la carpeta scanner.
     `n` 7: Ejecucion hostname (para saber nombre del equipo de red ponerlo en la impresora).
-    `n` 8: Salir.
+    `n`####################################################################################################################
+    `n`#                                                                                                                  #
+    `n`#                                         INSTALACIONES_APLICACIONES (8-12).                                       #
+    `n`#                                                                                                                  #
+    `n`####################################################################################################################
+    `n` 8: Instalar INSTALACIOR CHOCO.
+    `n` 9: Instalar TODO_APLICACONES.
+    `n` 10: Instalar REMOTAS.
+    `n` 11: Instalar TODO_SIN_ONLIOFFICE.
+    `n` 12: DESISTALACION TODO.
+    `n` 13: SALIR
     `n` Opcion"
    switch ($repuesta)
    {
@@ -40,7 +55,60 @@ function menu
          }
        6{Remove-SmbShare -Name scanner}
        7{hostname}
-       8{exit}
+       8{
+           Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))        
+       }
+       9{
+           choco install chrome-remote-desktop-chrome -y --force
+           choco install firefox -y --force
+           choco install winrar -y --force
+           choco install open-shell -y --force
+           choco install vlc-skins -y --force
+           choco install gimp -y --force
+           choco install adobereader -y --force
+           choco install onlyoffice -y --force
+           choco install audacity -y --force
+           choco install imgburn -y --force
+           choco install 7zip.install -y --force
+           choco install teamviewer -y --force
+           choco install anydesk -y --force
+          
+       }
+       10{
+           choco install teamviewer -y --force
+           choco install anydesk -y --force
+       }
+       11{
+           choco install chrome-remote-desktop-chrome -y --force
+           choco install firefox -y --force
+           choco install winrar -y --force
+           choco install open-shell -y --force
+           choco install vlc-skins -y --force
+           choco install gimp -y --force
+           choco install adobereader -y --force
+           choco install audacity -y --force
+           choco install imgburn -y --force
+           choco install 7zip.install -y --force
+           choco install teamviewer -y --force
+           choco install anydesk -y --force
+       }
+       12{
+           choco uninstall chrome-remote-desktop-chrome -y --force
+           choco uninstall firefox -y --force
+           choco uninstall winrar -y --force
+           choco uninstall open-shell -y --force
+           choco uninstall vlc-skins -y --force
+           choco uninstall gimp -y --force
+           choco uninstall adobereader -y --force
+           choco uninstall onlyoffice -y --force
+           choco uninstall audacity -y --force
+           choco uninstall imgburn -y --force
+           choco uninstall 7zip.install -y --force
+           choco uninstall teamviewer -y --force
+           choco uninstall anydesk -y --force
+       
+       }
+       13{exit}
        Default {Write-Host "Opcion no valida"}
     }
    
